5b2ff3e15097deee618514a7d3734f98f16bea84
Martin <martdang5@gmail.com>
2023-09-28 09:53:56 -0400

    Started on the project got it all up and working, now on js part

---
+++ index.js
@@ -0,0 +1,6 @@
+const allNums = document.querySelectorAll('[data-number]');
+const allOperations = document.querySelectorAll('[data-operation]');
+
+allNums.forEach(button => {
+    button.addEventListener(onclick, x)
+})
