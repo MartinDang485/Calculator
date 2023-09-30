d7786a023b36e8a9587acbc40f9b0be5b9392970
Martin <martdang5@gmail.com>
2023-09-30 13:50:21 -0400

    Did the js! Project is complete, does not look the best, but I got the bare minimum of what I need to meet the standards of the project, which is to have a working calculator, will make improvments in future.

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-const allNums = document.querySelectorAll('[data-number]');
-const allOperations = document.querySelectorAll('[data-operation]');
 
-allNums.forEach(button => {
-    button.addEventListener(onclick, x)
+const allNums = document.querySelectorAll('button');
+const display = document.querySelector('#output');
+console.log(allNums);
+
+let array = [];
+let concatenated;
+
+allNums.forEach(button => { //for each to add eventlisteners to all buttons, and to link a callback function
+    button.addEventListener('click', () => calculate(button));
 })
+
+function calculate(button) {
+    const value = button.textContent; //takes content of button
+    
+    if(button.textContent == "AC") { //if clicked AC, clears display
+        array = []; //clears array
+        display.textContent = ".";
+    }
+    else if(button.textContent == "=") { //if equals
+        concatenated = array.join(''); //join array
+        display.textContent = eval(concatenated); //evaluate expression in array
+    }
+    else if(button.textContent == "DEL") {
+        array.pop();
+        display.textContent = array.join('');
+    }
+    else { //else, we keep pushing values until top 2 conditions are met
+        array.push(value);
+        display.textContent = array.join('');
+        console.log(array);
+    }
+}
+
+
